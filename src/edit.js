aea3a7f3c1ef5785414b831c65a5e4b3518b11bb
rodrigo <rodrigo@cakedidit.com>
2023-03-17 09:24:31 -0300

    Change query args, filter by cat, etc. Final fixes

--- src/edit.js
+++ src/edit.js
@@ -1,11 +1,145 @@
 import { __ } from '@wordpress/i18n';
-import { useBlockProps } from '@wordpress/block-editor';
+import { RawHTML } from '@wordpress/element';
+import { format, dateI18n, getSettings } from '@wordpress/date';
+import { useBlockProps, InspectorControls } from '@wordpress/block-editor';
+import { useSelect } from '@wordpress/data';
+import { PanelBody, ToggleControl, QueryControls } from '@wordpress/components';
 import './editor.scss';
 
-export default function Edit() {
+export default function Edit({ attributes, setAttributes }) {
+	const { numberOfPosts, displayFeatureImage, order, orderBy, categories } =
+		attributes;
+
+	const categoriesIDs =
+		categories && categories.length > 0
+			? categories.map((category) => category.id)
+			: [];
+
+	const posts = useSelect(
+		(select) => {
+			return select('core').getEntityRecords('postType', 'post', {
+				per_page: numberOfPosts,
+				_embed: true,
+				order,
+				orderby: orderBy,
+				categories: categoriesIDs,
+			});
+		},
+		[numberOfPosts, order, orderBy, categories]
+	);
+
+	const allCategories = useSelect((select) => {
+		return select('core').getEntityRecords('taxonomy', 'category', {
+			per_page: -1,
+		});
+	}, []);
+
+	const categorySuggestions = {};
+	if (allCategories) {
+		for (let i = 0; i < allCategories.length; i++) {
+			const category = allCategories[i];
+			categorySuggestions[category.name] = category;
+		}
+	}
+
+	const onDisplayFeatureImageChange = (value) => {
+		setAttributes({ displayFeatureImage: value });
+	};
+
+	const onNumberOfItemsChange = (value) => {
+		setAttributes({ numberOfPosts: value });
+	};
+
+	const onCategoryChange = (values) => {
+		const hasNoSuggestions = values.some(
+			(value) => typeof value === 'string' && !categorySuggestions[value]
+		);
+
+		if (hasNoSuggestions) return;
+
+		const updatedCategories = values.map((token) => {
+			return typeof token === 'string'
+				? categorySuggestions[token]
+				: token;
+		});
+
+		setAttributes({ categories: updatedCategories });
+	};
+
 	return (
-		<p {...useBlockProps()}>
-			{__('Boilerplate – hello from the editor!', 'boilerplate')}
-		</p>
+		<>
+			<InspectorControls>
+				<PanelBody>
+					<ToggleControl
+						label={__('Display feature image', 'latest-posts')}
+						checked={displayFeatureImage}
+						onChange={onDisplayFeatureImageChange}
+					/>
+					<QueryControls
+						numberOfItems={numberOfPosts}
+						onNumberOfItemsChange={onNumberOfItemsChange}
+						maxItems={10}
+						minItems={1}
+						orderBy={orderBy}
+						onOrderByChange={(value) =>
+							setAttributes({ orderBy: value })
+						}
+						order={order}
+						onOrderChange={(value) =>
+							setAttributes({ order: value })
+						}
+						categorySuggestions={categorySuggestions}
+						onCategoryChange={onCategoryChange}
+						selectedCategories={categories}
+					/>
+				</PanelBody>
+			</InspectorControls>
+			<ul {...useBlockProps()}>
+				{posts &&
+					posts.map((post) => {
+						const featuredImage =
+							post._embedded &&
+							post._embedded['wp:featuredmedia'] &&
+							post._embedded['wp:featuredmedia'].length > 0 &&
+							post._embedded['wp:featuredmedia'][0];
+
+						return (
+							<li key={post.id}>
+								{displayFeatureImage && featuredImage && (
+									<img
+										src={
+											featuredImage.media_details.sizes
+												.large.source_url
+										}
+										alt={featuredImage.alt_text}
+									></img>
+								)}
+								<h5>
+									<a href={post.link}>
+										{post.title.rendered ? (
+											<RawHTML>
+												{post.title.rendered}
+											</RawHTML>
+										) : (
+											__('No title', 'latest-posts')
+										)}
+									</a>
+								</h5>
+								{post.date_gmt && (
+									<time dateTime={format('c', post.date_gmt)}>
+										{dateI18n(
+											getSettings().formats.date,
+											post.date_gmt
+										)}
+									</time>
+								)}
+								{post.excerpt.rendered && (
+									<RawHTML>{post.excerpt.rendered}</RawHTML>
+								)}
+							</li>
+						);
+					})}
+			</ul>
+		</>
 	);
 }
